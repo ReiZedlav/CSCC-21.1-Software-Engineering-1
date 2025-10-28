f64fa94017c1d53ddfe000050fe0d3717d0066ef
Rei Zedlav <andreialbertsonvaldez123@gmail.com>
2025-10-28 16:10:16 +0800

    DB Milestone 1

--- POS/Source Code/Database/db.sql
+++ POS/Source Code/Database/db.sql
@@ -1,10 +1,36 @@
 
-//plan
+CREATE DATABASE POS
 
-1. create first Users db.
-2. then roles.
+USE POS
+
+//to do list
+
+1. create first Users db. DONE
+2. then roles. DONE
+
+CREATE TABLE Roles(
+    roleId INT AUTO_INCREMENT PRIMARY KEY,
+    roleName varchar(100)
+);
+
+//insert into roles Cashier and Administrator
+
+CREATE TABLE Users(
+    userID INT AUTO_INCREMENT PRIMARY KEY,
+    firstName varchar(100),
+    lastName varchar(100),
+    userName varchar(100) UNIQUE,
+    HashedPassword varchar(100),
+    roleId INT
+
+    FOREIGN KEY (roleId) REFERENCES Roles(roleId)
+);
 
 3. Add a default Administrator and strong password default...
+
+
+
+
 
 4. Create products db
 5. Create category db
